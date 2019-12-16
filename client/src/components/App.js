2d281cd9401474f11b52f29e16d2c14ec2b6b423
Jaskaranpreet Singh <jaskaranpreetsingh@JaskarareetsMBP.attlocal.net>
2019-12-15 23:16:20 -0800

    initial commit

---
+++ client/src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../style/App.css';
+import ProfilePicture from './ProfilePicture';
+
+function App() {
+  return (
+    <div className="app-container">
+      <ProfilePicture/>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/ProfilePicture.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../style/ProfilePicture.css';
+
+export default function ProfilePicture() {
+    return (
+        <div className="profile-container">
+            <div className="profile-picture">
+                {/* <img/> */}
+            </div>
+        </div>
+    )
+}
