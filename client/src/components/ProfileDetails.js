4199fec5e8319a65e72de802003584ae00c64ca0
Jaskaranpreet Singh <jaskaranpreetsingh@JaskarareetsMBP.attlocal.net>
2019-12-23 00:20:29 -0800

    added profile details component

---
+++ client/src/components/ProfileDetails.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../style/ProfileDetails.css';
+
+export default function ProfileDetails() {
+    return (
+        <div className="profile-detail-container">
+            <h1>Profile details </h1>
+        </div>
+    )
+}
