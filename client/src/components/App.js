da7ebe5468ec2c1fd495e6456bd3a0e8461294de
Jaskaranpreet Singh <jaskaranpreetsingh@JaskarareetsMBP.attlocal.net>
2019-12-23 00:21:06 -0800

    refactor style and imported profiledetails comp

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../style/App.css';
 import ProfilePicture from './ProfilePicture';
+import ProfileDetails from './ProfileDetails';
 
 function App() {
   return (
     <div className="app-container">
       <ProfilePicture/>
+      <ProfileDetails/>
     </div>
   );
 }
