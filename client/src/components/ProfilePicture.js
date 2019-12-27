740c474e67dffc0ed7f1253a2e3cf5ab38a74a03
Jaskaranpreet Singh <jaskaranpreetsingh@JaskarareetsMBP.attlocal.net>
2019-12-26 23:30:51 -0800

    added upload functionality

--- client/src/components/ProfilePicture.js
+++ client/src/components/ProfilePicture.js
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useState ,Fragment } from 'react'
 import '../style/ProfilePicture.css';
 
 export default function ProfilePicture() {
+    const [profilePic , setProfilePic] = useState('')
+    const handlePhoto = (event) =>{
+        let uploadedImg = (event !==null) ? URL.createObjectURL(event.target.files[0]) :''
+        setProfilePic(uploadedImg)
+    }
     return (
+        <Fragment>
             <div className="profile-container">
                 <div className="profile-picture">
-                    {/* <img/> */}
+                    {profilePic.length === 0 ? '': <img alt="Profile Pic" src={profilePic}/>}
+                </div>
+                <div className="upload-button">
+                    <input type="file"  id="file" onChange={(e)=>handlePhoto(e)}/>
+                    <label htmlFor="file">Upload Picture</label>
                 </div>
             </div>
+
+        </Fragment>
+
     )
 }
