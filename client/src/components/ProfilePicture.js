40caad9a78ff45a396178eefa183ceca08b040c8
Jaskaranpreet Singh <jaskaranpreetsingh@MacBook-Pro-2.attlocal.net>
2020-01-02 14:56:17 -0800

    storing image in firebase

--- client/src/components/ProfilePicture.js
+++ client/src/components/ProfilePicture.js
@@ -1,11 +1,18 @@
 import React, { useState ,Fragment } from 'react'
 import '../style/ProfilePicture.css';
+import axios from 'axios'
+
 
 export default function ProfilePicture() {
     const [profilePic , setProfilePic] = useState('')
     const handlePhoto = (event) =>{
         let uploadedImg = (event !==null) ? URL.createObjectURL(event.target.files[0]) :''
+        uploadedImage()
         setProfilePic(uploadedImg)
+    }
+
+    const uploadedImage = () => {
+        axios.post(`${process.env.REACT_APP_DATABASE_URL}/profile.json` , {image:profilePic})
     }
     return (
         <Fragment>
@@ -14,7 +21,7 @@ export default function ProfilePicture() {
                     {profilePic.length === 0 ? '': <img alt="Profile Pic" src={profilePic}/>}
                 </div>
                 <div className="upload-button">
-                    <input type="file"  id="file" onChange={handlePhoto}/>
+                    <input type="file" id="file" onChange={handlePhoto}/>
                     <label htmlFor="file">Upload Picture</label>
                 </div>
             </div>
