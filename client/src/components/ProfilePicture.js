03f68e2b490f80e5e86ee96e2b8e42772bfcfefb
Jaskaranpreet Singh <jaskaranpreetsingh@MacBook-Pro-2.local>
2020-01-25 12:03:53 -0600

    borken code for storing image in firbase and retriving back

--- client/src/components/ProfilePicture.js
+++ client/src/components/ProfilePicture.js
@@ -4,15 +4,30 @@ import axios from 'axios'
 
 
 export default function ProfilePicture() {
+    const [rawImage, setRawImage] = useState('')
     const [profilePic , setProfilePic] = useState('')
     const handlePhoto = (event) =>{
         let uploadedImg = (event !==null) ? URL.createObjectURL(event.target.files[0]) :''
         uploadedImage()
         setProfilePic(uploadedImg)
+        setRawImage(event.target.files[0])
     }
 
     const uploadedImage = () => {
-        axios.post(`${process.env.REACT_APP_DATABASE_URL}/profile.json` , {image:profilePic})
+        const getBase64Image = (img) => {
+        let canvas = document.createElement("canvas");
+        canvas.width = img.width;
+        canvas.height = img.height;
+
+        let ctx = canvas.getContext("2d");
+        ctx.drawImage(img, 0, 0);
+
+        let dataURL = canvas.toDataURL("image/png");
+
+        return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+    }
+        console.log('rawImage', rawImage )
+        axios.post(`${process.env.REACT_APP_DATABASE_URL}/profile.json` , {image:rawImage})
     }
     return (
         <Fragment>
