76f4d829942cc35205a0c8340e176bea0a323e29
Jaskaranpreet Singh <jaskaranpreetsingh@MacBook-Pro-2.local>
2020-01-26 17:35:30 -0600

    used props from parent comp

--- client/src/components/ProfilePicture.js
+++ client/src/components/ProfilePicture.js
@@ -1,46 +1,25 @@
 import React, { useState ,Fragment } from 'react'
 import '../style/ProfilePicture.css';
-import axios from 'axios'
 
-
-export default function ProfilePicture() {
-    const [rawImage, setRawImage] = useState('')
+export default function ProfilePicture(props) {
     const [profilePic , setProfilePic] = useState('')
     const handlePhoto = (event) =>{
         let uploadedImg = (event !==null) ? URL.createObjectURL(event.target.files[0]) :''
-        uploadedImage()
+        props.uploadedImage(event)
         setProfilePic(uploadedImg)
-        setRawImage(event.target.files[0])
     }
 
-    const uploadedImage = () => {
-        const getBase64Image = (img) => {
-        let canvas = document.createElement("canvas");
-        canvas.width = img.width;
-        canvas.height = img.height;
-
-        let ctx = canvas.getContext("2d");
-        ctx.drawImage(img, 0, 0);
-
-        let dataURL = canvas.toDataURL("image/png");
-
-        return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
-    }
-        console.log('rawImage', rawImage )
-        axios.post(`${process.env.REACT_APP_DATABASE_URL}/profile.json` , {image:rawImage})
-    }
     return (
         <Fragment>
             <div className="profile-container">
                 <div className="profile-picture">
-                    {profilePic.length === 0 ? '': <img alt="Profile Pic" src={profilePic}/>}
+                    {profilePic.length === 0 ? <img alt="Profile Pic" src={props.img}/>: <img alt="Profile Pic" src={profilePic}/>}
                 </div>
                 <div className="upload-button">
                     <input type="file" id="file" onChange={handlePhoto}/>
                     <label htmlFor="file">Upload Picture</label>
                 </div>
             </div>
-
         </Fragment>
 
     )
