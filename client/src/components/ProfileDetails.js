7dd7200632e0da3b678f389152b09cf6d7b82f5e
Jaskaranpreet Singh <jaskaranpreetsingh@MacBook-Pro-2.local>
2020-01-26 17:34:30 -0600

    removed commentd code

--- client/src/components/ProfileDetails.js
+++ client/src/components/ProfileDetails.js
@@ -1,25 +1,7 @@
-import React, { useState , useEffect } from 'react'
+import React from 'react'
 import '../style/ProfileDetails.css';
-import axios from 'axios';
 
 export default function ProfileDetails() {
-    // const [recentImage, setRecentImage] = useState('')  
-    // const setImage = (data) => {
-    //     console.log(data['-LxV6Y9-Um5A-ji4UK_y'].image)
-    //     axios({
-    //         method: 'get',
-    //         url: data['-LxV6Y9-Um5A-ji4UK_y'].image,
-    //         responseType: 'blob'
-    //     }).then((response)=>{
-    //         console.log('rep',response)
-    //         let reader = new FileReader();
-    //         reader.readAsDataURL(response.data); 
-    //         reader.onloadend = function() {
-    //             let base64data = reader.result;
-    //         }
-    //     })
-    //     // setRecentImage(data['-LxV6Y9-Um5A-ji4UK_y'].image)
-    // }
     return (
         <div className="profile-detail-container">
             <div className="profile-detail">
@@ -27,10 +9,10 @@ export default function ProfileDetails() {
                     Jaskaranpreet Singh
                 </div>
                 <div className="email">
-                    <h1>jaskaransingh123@gmail.com</h1>
+                    <div>jaskaransingh123@gmail.com</div>
                 </div>
                 <div className="phone">
-                    <h1>669-261-8960</h1>
+                    <div>669-261-8960</div>
                 </div> 
             </div>
         </div>
