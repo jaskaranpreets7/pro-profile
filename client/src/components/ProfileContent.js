667a4cce7496d5ae941b099ecc08354685c72bab
Jaskaranpreet Singh <jaskaranpreetsingh@MacBook-Pro-2.local>
2020-02-09 23:49:43 -0600

    added hooks and passin some props

--- client/src/components/ProfileContent.js
+++ client/src/components/ProfileContent.js
@@ -1,15 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../style/ProfileContent.css';
+import ProfileDrawer from './ProfileDrawer';
+import ProfileView from './ProfileView';
 
-export default function ProfileContent() {
+export default function ProfileContent(props) {
+    const labels = ['About Me', 'Experience' , 'Skills', 'Projects', 'Education']
+
+    const [value, setValue] = useState('')
+    const handleClick = (label) => {
+        console.log('in content ' , label)
+        setValue(label)
+    }
     return (
         <div className="profile-content-wrapper">
             <div className="profile-content-container">
                 <div className="profile-drawer">
-
+                    <ProfileDrawer handleClick={handleClick} labels={labels}/>
                 </div>
-                <div className="profile-text">
-
+                <div className="profile-view">
+                    <ProfileView text={value}/>
                 </div>
             </div>
         </div>
