da48c2a0ab2a6749bee368c4205c209f1e0e2e9f
Jaskaranpreet Singh <jaskaranpreetsingh@MacBook-Pro-2.local>
2020-03-03 00:01:16 -0600

    added axios call in app componet

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,6 +3,7 @@ import '../style/App.css';
 import ProfilePicture from './ProfilePicture';
 import ProfileDetails from './ProfileDetails';
 import firebase from 'firebase'
+import axios from "axios"
 import { config } from "../common/firebase.js";
 import ProfileContent from './ProfileContent';
 import Button from './Button';
@@ -14,24 +15,33 @@ class App extends Component{
     this.state = {
       img : ''
     }
+
+
   }
   componentDidMount(){
     firebase.initializeApp(config)
+    this.fetchData()
     this.getProfilePic()
   }
 
+  fetchData = async() => {
+    await axios.get('https://pro-profile-fb2e2.firebaseio.com/0.json')
+      .then((resp)=>console.log('reso',resp))
+  }
   uploadedImage = (event) => {
     const storage = firebase.storage()
+
     const image = event.target.files[0]
     localStorage.setItem('image', image.name)
     storage.ref(`images/${image.name}`).put(image)
   }
 
   getProfilePic = () => {
-    let reterivedImg = localStorage.getItem('image')
-    if(reterivedImg.length > 0 ){
-      const storage = firebase.storage()
-      storage.ref('images').child(reterivedImg).getDownloadURL()
+    const storage = firebase.storage()
+
+    // let reterivedImg = localStorage.getItem('image')
+    // if(reterivedImg && reterivedImg.length > 0 ){
+        storage.ref('images').child('IMG_3873.JPG').getDownloadURL()
         .then((img)=>{
           this.setState({
             img : img
@@ -40,7 +50,7 @@ class App extends Component{
         .catch((error)=>{
           console.log(error)
         })
-    }
+    // }
   }
   toLinkedin = () => {
     window.location.href = 'https://www.linkedin.com/in/jaskaranpreet-singh/'
