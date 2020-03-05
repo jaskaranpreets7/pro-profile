8ed0a91f0f4ac2825c007a1b024aeb8ba28b34ee
Jaskaranpreet Singh <jaskaranpreetsingh@MacBook-Pro-2.local>
2020-03-04 23:38:03 -0600

    used data from props

--- client/src/components/ProfileDetails.js
+++ client/src/components/ProfileDetails.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import '../style/ProfileDetails.css';
 
-export default function ProfileDetails() {
+export default function ProfileDetails({userProfileData}) {
     return (
         <div className="profile-detail-container">
             <div className="profile-detail">
                 <div className="name">
-                    Jaskaranpreet Singh
+                    {userProfileData.first_name + ' ' + userProfileData.last_name}
                 </div>
                 <div className="email">
-                    <div>jaskaransingh123@gmail.com</div>
+                    <div>{userProfileData.email}</div>
                 </div>
                 <div className="phone">
-                    <div>669-261-8960</div>
+                    <div>{userProfileData.phone}</div>
                 </div> 
             </div>
         </div>
