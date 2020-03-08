87216d6c7d39e82ff4688eb065ce4cd12af33c74
Jaskaranpreet Singh <jaskaranpreetsingh@MacBook-Pro-2.local>
2020-03-08 18:15:47 -0500

    storeing data in local component state . functionality broken to display data

--- client/src/components/ProfileView.js
+++ client/src/components/ProfileView.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
-export default function ProfileView(props) {
+export default function ProfileView({label , userProfileContent}) {
+    const [data , setData] = useState('')
+    const storeData = () => {
+        if(label.toLowerCase() in userProfileContent){
+            setData(userProfileContent[label.toLowerCase()])
+        }
+    }
+    useEffect(()=>{
+        storeData()
+    })
+
     return (
         <div>
-            <h1>{props.text}</h1>
+            {(data.length && data.length > 0 )?
+            data.map((item)=> {
+                return (
+                    <li>
+                        <ul>
+                            {item.company}
+                        </ul>
+                    </li>
+                )
+            })
+            :
+            null
+            }
         </div>
     )
 }
