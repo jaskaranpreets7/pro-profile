e80a8dce7d72ad60718a7bf2fa24014e9d3fd52f
Jaskaranpreet Singh <jaskaranpreetsingh@MacBook-Pro-2.local>
2020-03-11 00:13:45 -0500

    added project comp

---
+++ client/src/components/Projects.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Projects({projectContent}) {
+    console.log(projectContent)
+
+    return (
+        <div>
+            Projects
+        </div>
+    )
+}
