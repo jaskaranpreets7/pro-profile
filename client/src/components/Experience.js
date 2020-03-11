e7c361ace37fb81d8819a80f8645096dd6da895e
Jaskaranpreet Singh <jaskaranpreetsingh@MacBook-Pro-2.local>
2020-03-11 00:13:14 -0500

    added Experience comp

---
+++ client/src/components/Experience.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Experience({experienceContent}) {
+    console.log(experienceContent)
+    return (
+        <div>
+            Experience
+        </div>
+    )
+}
