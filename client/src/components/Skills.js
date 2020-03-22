a45c1552ab5732fecc97d1c172a3a2762b43bba0
Jaskaranpreet Singh <jaskaranpreetsingh@MacBook-Pro-2.local>
2020-03-21 22:38:22 -0700

    mapped skills

--- client/src/components/Skills.js
+++ client/src/components/Skills.js
@@ -1,9 +1,18 @@
 import React from 'react'
-
+import '../style/Skills.css'
 export default function Skills({skillContent}) {
+    console.log(skillContent)
     return (
-        <div>
-            Skills
+        <div className="skills-wrapper">
+            {skillContent.map((item)=>{
+                return (
+                    <div className="bubble">
+                        <div className="text">
+                            {item.name}
+                        </div>
+                    </div>
+                )
+            })}
         </div>
     )
 }
