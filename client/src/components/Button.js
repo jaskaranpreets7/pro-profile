1ccad3a46d5a3b9880d0f28c1b5e45898f109a1a
Jaskaranpreet Singh <jaskaranpreetsingh@MacBook-Pro-2.local>
2020-04-12 18:59:55 -0700

    removed inline-styling

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -2,5 +2,5 @@ import React from 'react'
 import '../style/button.scss'
 
 export default function Button(props) {
-    return ( <i style={{fontSize: '50px' , padding: '15px 0px 0px 0px'}} className={props.className} onClick={()=>props.handleClick()}></i> )
+    return ( <i className={props.className} onClick={()=>props.handleClick()}></i> )
 }
