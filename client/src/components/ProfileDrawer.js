3ff27d4b95c7899a06f78a9f08444c68f4050360
Jaskaranpreet Singh <jaskaranpreetsingh@MacBook-Pro-2.local>
2020-04-16 23:32:41 -0700

    added autoClicked funtionality

--- client/src/components/ProfileDrawer.js
+++ client/src/components/ProfileDrawer.js
@@ -6,7 +6,19 @@ export default function ProfileDrawer(props) {
     const handleClick = (label) => {
         props.handleClick(label)
     }
-    const renderLabels = props.labels.map( (label , index )=><button type="button" className="btn btn-primary btn-lg btn-block" key={index} onClick={()=> handleClick(label)}>{label}</button> )
+    const renderLabels = props.labels.map( (label , index ) => {
+        return (
+            <button 
+                type="button" 
+                className="btn btn-primary btn-lg btn-block" 
+                key={index} onClick={()=> handleClick(label)} 
+                onFocusCapture={()=> handleClick(label)}
+                autoFocus={label === "About Me" ? true : false}
+                >
+                    {label}
+            </button> 
+        )
+    })
 
     return (
         <div className="profile-drawer-wrapper">
